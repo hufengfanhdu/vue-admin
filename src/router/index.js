5a5ab2f6e0cb1885037f8d51ff09fa0e9c52b382
FrankHu <471048894@qq.com>
2020-07-25 14:27:36 +0800

    day1

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    redirect: "login"
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: () => import("../views/Login/index")
+  }
+];
+
+const router = new VueRouter({
+  routes
+});
+
+export default router;
---
+++ src/views/Login/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="login">
+        <el-button>默认按钮</el-button>
+    </div>
+</template>
+
+<script>
+    export default{}
+</script>
+
+<style lang="scss" scoped>
+    #login {
+        height: 100vh;
+        background: #3f556b;
+    }
+</style>
